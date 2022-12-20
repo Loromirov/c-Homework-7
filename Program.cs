ad7a15bd30540fca4b6bd9489ac9e619bf301834
Loromirov <loromirov@yandex.ru>
2022-12-21 00:30:33 +0300

    третья задача

--- Program.cs
+++ Program.cs
@@ -1,70 +1,43 @@
 ﻿/*
-Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-и возвращает значение этого элемента или же указание, что такого элемента нет.
-Например, задан массив:
+ Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
+Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
-
-17 -> такого числа в массиве нет
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
-int GetNumber(string message)
+
+
+
+int[,] arr = {
+    { 1, 4, 7, 2 },
+    { 5, 9, 2, 3 },
+    { 8, 4, 2, 4 }
+};
+void PrintMatrix(int[,] arr)
 {
-    int result = 0;
-
-    while(true)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        Console.WriteLine(message);
-
-        if(int.TryParse(Console.ReadLine(), out result))
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Ввели не число");
-        }
-    }
-
-    return result;
-}
-int[,] FillMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i,j] = new Random().Next(1, 10);
-        }
-    } 
-    return matrix;
-}
-
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i,j]} ");
+            Console.Write($"{arr[i,j]} ");
         }
         Console.WriteLine();
     }
 
     Console.WriteLine();
 }
-int rows = GetNumber("Введите номер строки");
-int columns = GetNumber("Введите номер столбца");
-int[,] matrix = new int[4,4];
-FillMatrix(matrix);
-PrintMatrix(matrix);
-if (rows < matrix.GetLength(0) && columns < matrix.GetLength(1))
-{
-     Console.WriteLine(matrix[rows, columns]);
-}
+PrintMatrix(arr);
 
-else
- {
-    Console.WriteLine($"{rows},{columns} -> такого числа в массиве нет");
- }
+Console.WriteLine("---------------------------");
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ (sum / arr.GetLength(0) * 10) / 10} ");
+}
+Console.ReadLine();
